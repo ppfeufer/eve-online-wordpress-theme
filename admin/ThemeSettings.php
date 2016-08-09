74fb739d16b010b8307264f9d6c9073ca569abe4
Peter Pfeufer <info@ppfeufer.de>
2016-08-09 16:12:30 +0200

    simplified theme settings array

--- admin/ThemeSettings.php
+++ admin/ThemeSettings.php
@@ -31,33 +31,7 @@ class ThemeSettings {
 				'general-settings' => array(
 					'tab_title' => \__('General Settings', 'eve-online'),
 					'tab_description' => \__('General Theme Settings', 'eve-online'),
-					'fields' => array(
-						'type' => array(
-							'type' => 'select',
-							'choices' => array(
-								'alliance' => \__('Alliance', 'eve-online'),
-								'corporation' => \__('Corporation', 'eve-online')
-							),
-							'empty' => \__('Please Select', 'eve-online'),
-							'title' => \__('Entity Type', 'eve-online'),
-							'description' => \__('Is this a Corporation or an Alliance Website?', 'eve-online')
-						),
-						'name' => array(
-							'type' => 'text',
-							'title' => \__('Entity Name', 'eve-online'),
-							'description' => \sprintf(\__('The Name of your Corp/Alliance %1$s', 'eve-online'),
-								(!empty($this->themeOptions['name'])) ? '</p></td></tr><tr><th>' . \__('Your Logo', 'eve-online') . '</th><td>' . $this->eveApi->getEntityLogoByName($this->themeOptions['name'], false) : ''
-							)
-						),
-						'show_corp_logos' => array(
-							'type' => 'checkbox',
-							'title' => \__('Corp Logos', 'eve-online'),
-							'choices' => array(
-								'show' => \__('Show corp logos in menu for corp pages.', 'eve-online')
-							),
-							'description' => \__('Only available if you are running an alliance website, so you can have the corp logos in your "Our Corporations" menu.', 'eve-online')
-						),
-					),
+					'fields' => $this->getGeneralTabFields()
 				),
 
 				/**
@@ -66,29 +40,7 @@ class ThemeSettings {
 				'background-settings' => array(
 					'tab_title' => \__('Background Settings', 'eve-online'),
 					'tab_description' => \__('Background Settings', 'eve-online'),
-					'fields' => array(
-						'use_background_image' => array(
-							'type' => 'checkbox',
-							'title' => \__('Use Background Image', 'eve-online'),
-							'choices' => array(
-								'yes' => \__('Yes, I want to use background images on this website.', 'eve-online')
-							),
-							'description' => \__('If this option is checked, the website will use your selected (down below) background image instead of a simple colored background.', 'eve-online')
-						),
-						'background_image' => array(
-							'type' => 'radio',
-							'choices' => EveOnline\eve_get_default_background_images(true),
-							'empty' => \__('Please Select', 'eve-online'),
-							'title' => \__('Background Image', 'eve-online'),
-							'description' => \__('Select one of the default Background images ...', 'eve-online'),
-							'align' => 'horizontal'
-						),
-						'background_image_upload' => array(
-							'type' => 'image',
-							'title' => \__('', 'eve-online'),
-							'description' => \__('... or upload your own', 'eve-online')
-						)
-					)
+					'fields' => $this->getBackgroundTabFields()
 				),
 
 				/**
@@ -97,28 +49,7 @@ class ThemeSettings {
 				'slider-settings' => array(
 					'tab_title' => \__('Slider Settings', 'eve-online'),
 					'tab_description' => \__('Slider Settings', 'eve-online'),
-					'fields' => array(
-						/**
-						 * !!!
-						 * Do NOT forget to change the options key in
-						 * metaslider-plugin as well
-						 * !!!
-						 */
-						'default_slider' => array(
-							'type' => 'select',
-							'title' => \__('Default Slider on Front Page', 'eve-online'),
-							'choices' => $this->metaSlider->metasliderGetOptions(),
-							'description' => ($this->metaSlider->metasliderPluginExists()) ? \__('Select the default slider for your front page', 'eve-online') : \sprintf(\__('Please make sure you have the %1$s plugin installed and activated.', 'eve-online'), '<a href="https://wordpress.org/plugins/ml-slider/">Meta Slider</a>')
-						),
-						'default_slider_on' => array(
-							'type' => 'checkbox',
-							'title' => \__('Pages with Slider', 'eve-online'),
-							'choices' => array(
-								'frontpage_only' => \__('Show only on front page.', 'eve-online')
-							),
-							'description' => \__('Show this slider only on front page in case no other slider is defined.', 'eve-online')
-						),
-					)
+					'fields' => $this->getSliderTabFields()
 				)
 			)
 		);
@@ -127,34 +58,119 @@ class ThemeSettings {
 			$themeOptionsPage['eve-online-theme-settings']['tabs']['development'] = array(
 				'tab_title' => \__('Development Infos', 'eve-online'),
 				'tab_description' => \__('Delevopment Information', 'eve-online'),
-				'fields' => array(
-					'eve_theme_options_sane' => array(
-						'type' => 'custom',
-						'content' => '<pre>' . \print_r(EveOnline\eve_get_options_default(), true) . '</pre>',
-						'title' => \__('Options Array<br>(sane from functions.php)', 'eve-online'),
-						'callback' => null,
-						'description' => \__('This are the sane options defined in functions.php via <code>\WordPress\Themes\EveOnline\eve_get_options_default()</code>', 'eve-online')
-					),
-					'eve_theme_options_from_db' => array(
-						'type' => 'custom',
-						'content' => '<pre>' . \print_r(\get_option('eve_theme_options'), true) . '</pre>',
-						'title' => \__('Options Array<br>(from DB)', 'eve-online'),
-						'callback' => null,
-						'description' => \__('This are the options from our database via <code>\get_option(\'eve_theme_options\')</code>', 'eve-online')
-					),
-					'eve_theme_options_merged' => array(
-						'type' => 'custom',
-						'content' => '<pre>' . \print_r(\get_option('eve_theme_options', EveOnline\eve_get_options_default()), true) . '</pre>',
-						'title' => \__('Options Array<br>(merged / used for Theme)', 'eve-online'),
-						'callback' => null,
-						'description' => \__('This are the options used for the theme via <code>\get_option(\'eve_theme_options\', \WordPress\Themes\EveOnline\eve_get_options_default())</code>', 'eve-online')
-					)
-				)
+				'fields' => $this->getDevelopmentTabFields()
 			);
 		} // END if(\preg_match('/development/', \APPLICATION_ENV))
 
 		return $themeOptionsPage;
 	} // END public function renderSettingsPage()
+
+	private function getGeneralTabFields() {
+		return array(
+			'type' => array(
+				'type' => 'select',
+				'choices' => array(
+					'alliance' => \__('Alliance', 'eve-online'),
+					'corporation' => \__('Corporation', 'eve-online')
+				),
+				'empty' => \__('Please Select', 'eve-online'),
+				'title' => \__('Entity Type', 'eve-online'),
+				'description' => \__('Is this a Corporation or an Alliance Website?', 'eve-online')
+			),
+			'name' => array(
+				'type' => 'text',
+				'title' => \__('Entity Name', 'eve-online'),
+				'description' => \sprintf(\__('The Name of your Corp/Alliance %1$s', 'eve-online'),
+					(!empty($this->themeOptions['name'])) ? '</p></td></tr><tr><th>' . \__('Your Logo', 'eve-online') . '</th><td>' . $this->eveApi->getEntityLogoByName($this->themeOptions['name'], false) : ''
+				)
+			),
+			'show_corp_logos' => array(
+				'type' => 'checkbox',
+				'title' => \__('Corp Logos', 'eve-online'),
+				'choices' => array(
+					'show' => \__('Show corp logos in menu for corp pages.', 'eve-online')
+				),
+				'description' => \__('Only available if you are running an alliance website, so you can have the corp logos in your "Our Corporations" menu.', 'eve-online')
+			),
+		);
+	} // END private function getGeneralTabFields()
+
+	private function getBackgroundTabFields() {
+		return array(
+			'use_background_image' => array(
+				'type' => 'checkbox',
+				'title' => \__('Use Background Image', 'eve-online'),
+				'choices' => array(
+					'yes' => \__('Yes, I want to use background images on this website.', 'eve-online')
+				),
+				'description' => \__('If this option is checked, the website will use your selected (down below) background image instead of a simple colored background.', 'eve-online')
+			),
+			'background_image' => array(
+				'type' => 'radio',
+				'choices' => EveOnline\eve_get_default_background_images(true),
+				'empty' => \__('Please Select', 'eve-online'),
+				'title' => \__('Background Image', 'eve-online'),
+				'description' => \__('Select one of the default Background images ...', 'eve-online'),
+				'align' => 'horizontal'
+			),
+			'background_image_upload' => array(
+				'type' => 'image',
+				'title' => \__('', 'eve-online'),
+				'description' => \__('... or upload your own', 'eve-online')
+			)
+		);
+	} // END private function getBackgroundTabFields()
+
+	private function getSliderTabFields() {
+		return array(
+			/**
+			 * !!!
+			 * Do NOT forget to change the options key in
+			 * metaslider-plugin as well
+			 * !!!
+			 */
+			'default_slider' => array(
+				'type' => 'select',
+				'title' => \__('Default Slider on Front Page', 'eve-online'),
+				'choices' => $this->metaSlider->metasliderGetOptions(),
+				'description' => ($this->metaSlider->metasliderPluginExists()) ? \__('Select the default slider for your front page', 'eve-online') : \sprintf(\__('Please make sure you have the %1$s plugin installed and activated.', 'eve-online'), '<a href="https://wordpress.org/plugins/ml-slider/">Meta Slider</a>')
+			),
+			'default_slider_on' => array(
+				'type' => 'checkbox',
+				'title' => \__('Pages with Slider', 'eve-online'),
+				'choices' => array(
+					'frontpage_only' => \__('Show only on front page.', 'eve-online')
+				),
+				'description' => \__('Show this slider only on front page in case no other slider is defined.', 'eve-online')
+			),
+		);
+	} // END private function getSliderTabFields()
+
+	private function getDevelopmentTabFields() {
+		return array(
+			'eve_theme_options_sane' => array(
+				'type' => 'custom',
+				'content' => '<pre>' . \print_r(EveOnline\eve_get_options_default(), true) . '</pre>',
+				'title' => \__('Options Array<br>(sane from functions.php)', 'eve-online'),
+				'callback' => null,
+				'description' => \__('This are the sane options defined in functions.php via <code>\WordPress\Themes\EveOnline\eve_get_options_default()</code>', 'eve-online')
+			),
+			'eve_theme_options_from_db' => array(
+				'type' => 'custom',
+				'content' => '<pre>' . \print_r(\get_option('eve_theme_options'), true) . '</pre>',
+				'title' => \__('Options Array<br>(from DB)', 'eve-online'),
+				'callback' => null,
+				'description' => \__('This are the options from our database via <code>\get_option(\'eve_theme_options\')</code>', 'eve-online')
+			),
+			'eve_theme_options_merged' => array(
+				'type' => 'custom',
+				'content' => '<pre>' . \print_r(\get_option('eve_theme_options', EveOnline\eve_get_options_default()), true) . '</pre>',
+				'title' => \__('Options Array<br>(merged / used for Theme)', 'eve-online'),
+				'callback' => null,
+				'description' => \__('This are the options used for the theme via <code>\get_option(\'eve_theme_options\', \WordPress\Themes\EveOnline\eve_get_options_default())</code>', 'eve-online')
+			)
+		);
+	} // END private function getDevelopmentTabFields()
 } // END class ThemeSettings
 
-new ThemeSettings();
+new ThemeSettings;
