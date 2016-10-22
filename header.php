e1873673152b4f92a4e0b0cfa97b130c88293024
Rodrigo Brito <rig.10@hotmail.com>
2016-10-22 16:25:37 -0200

    Remove CSS enqueue from header

--- header.php
+++ header.php
@@ -8,11 +8,6 @@
 
 		<link rel="pingback" href="<?php \bloginfo('pingback_url'); ?>" />
 
-		<!--[if lt IE 9]>
-		<script src="<?php echo \get_template_directory_uri(); ?>/js/html5.min.js" type="text/javascript"></script>
-		<script src="<?php echo \get_template_directory_uri(); ?>/js/respond.min.js" type="text/javascript"></script>
-		<![endif]-->
-
 		<?php \wp_head(); ?>
 	</head>
 
