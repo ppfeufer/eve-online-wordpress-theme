a03d33bb72bc056bca4e9521296f9c86a9579200
Peter Pfeufer <info@ppfeufer.de>
2017-02-15 10:49:22 +0100

    Added some structure names

--- plugins/Killboard.php
+++ plugins/Killboard.php
@@ -257,11 +257,6 @@ class Killboard {
 			'Fortizar',
 			'Keepstar',
 
-			// Engineering Complexes
-			'Raitaru',
-			'Azbel',
-			'Sotiyo',
-
 			// POS Tower
 			'Amarr Control Tower',
 			'Amarr Control Tower Small',
@@ -286,11 +281,16 @@ class Killboard {
 			'Phase Inversion Battery',
 			'Small Artillery Battery',
 			'Small AutoCannon Battery',
+			'Small Beam Laser Battery',
+			'Medium Pulse Laser Battery',
 			'Silo',
 			'Spatial Destabilization Battery',
 			'Stasis Webification Battery',
 			'Warp Disruption Battery',
 			'Warp Scrambling Battery',
+
+			// Orbital Modules
+			'Customs Office'
 		);
 	} // END private function getStructureNames()
 } // END class Killboard
