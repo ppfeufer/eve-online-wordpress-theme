26607c634d255a2fa4621fd0c42dea74e24cb56a
Peter Pfeufer <info@ppfeufer.de>
2025-09-16 16:08:25 +0200

    [MISC] Cleanup

--- Sources/EsiClient/Model/Characters/CharacterId/Portrait.php
+++ Sources/EsiClient/Model/Characters/CharacterId/Portrait.php
@@ -1,132 +1,128 @@
 <?php
 
-/*
- * Copyright (C) 2018 ppfeufer
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 namespace Ppfeufer\Theme\EVEOnline\EsiClient\Model\Characters\CharacterId;
 
+/**
+ * Class Portrait
+ *
+ * Represents the portrait URLs for a character in various resolutions.
+ * Provides getter and setter methods for each resolution, ensuring secure HTTPS URLs.
+ *
+ * @package Ppfeufer\Theme\EVEOnline\EsiClient\Model\Characters\CharacterId
+ */
 class Portrait {
     /**
-     * px512x512
+     * URL for the 512x512 pixel portrait.
      *
      * @var string
      */
-    protected $px512x512 = null;
+    protected string $px512x512;
 
     /**
-     * px256x256
+     * URL for the 256x256 pixel portrait.
      *
      * @var string
      */
-    protected $px256x256 = null;
+    protected string $px256x256;
 
     /**
-     * px128x128
+     * URL for the 128x128 pixel portrait.
      *
      * @var string
      */
-    protected $px128x128 = null;
+    protected string $px128x128;
 
     /**
-     * px64x64
+     * URL for the 64x64 pixel portrait.
      *
      * @var string
      */
-    protected $px64x64 = null;
+    protected string $px64x64;
 
     /**
-     * getPx512x512
+     * Get the URL for the 512x512 pixel portrait.
      *
-     * @return string
+     * @return string The URL of the 512x512 pixel portrait.
      */
-    public function getPx512x512() {
+    public function getPx512x512(): string {
         return $this->px512x512;
     }
 
     /**
-     * setPx512x512
+     * Set the URL for the 512x512 pixel portrait.
+     * Ensures the URL uses HTTPS instead of HTTP.
      *
-     * @param string $px512x512
+     * @param string $px512x512 The URL of the 512x512 pixel portrait.
      */
-    protected function setPx512x512(string $px512x512) {
-        $this->px512x512 = \preg_replace('/http:\/\//', 'https://', $px512x512);
+    protected function setPx512x512(string $px512x512): void {
+        $this->px512x512 = preg_replace('/http:\/\//', 'https://', $px512x512);
     }
 
     /**
-     * getPx256x256
+     * Get the URL for the 256x256 pixel portrait.
      *
-     * @return string
+     * @return string The URL of the 256x256 pixel portrait.
      */
-    public function getPx256x256() {
+    public function getPx256x256(): string {
         return $this->px256x256;
     }
 
     /**
-     * setPx256x256
+     * Set the URL for the 256x256 pixel portrait.
+     * Ensures the URL uses HTTPS instead of HTTP.
      *
-     * @param string $px256x256
+     * @param string $px256x256 The URL of the 256x256 pixel portrait.
      */
-    protected function setPx256x256(string $px256x256) {
-        $this->px256x256 = \preg_replace('/http:\/\//', 'https://', $px256x256);
+    protected function setPx256x256(string $px256x256): void {
+        $this->px256x256 = preg_replace('/http:\/\//', 'https://', $px256x256);
     }
 
     /**
-     * getPx128x128
+     * Get the URL for the 128x128 pixel portrait.
      *
-     * @return string
+     * @return string The URL of the 128x128 pixel portrait.
      */
-    public function getPx128x128() {
+    public function getPx128x128(): string {
         return $this->px128x128;
     }
 
     /**
-     * setPx128x128
+     * Set the URL for the 128x128 pixel portrait.
+     * Ensures the URL uses HTTPS instead of HTTP.
      *
-     * @param string $px128x128
+     * @param string $px128x128 The URL of the 128x128 pixel portrait.
      */
-    protected function setPx128x128(string $px128x128) {
-        $this->px128x128 = \preg_replace('/http:\/\//', 'https://', $px128x128);
+    protected function setPx128x128(string $px128x128): void {
+        $this->px128x128 = preg_replace('/http:\/\//', 'https://', $px128x128);
     }
 
     /**
-     * getPx64x64
+     * Get the URL for the 64x64 pixel portrait.
      *
-     * @return string
+     * @return string The URL of the 64x64 pixel portrait.
      */
-    public function getPx64x64() {
+    public function getPx64x64(): string {
         return $this->px64x64;
     }
 
     /**
-     * setPx64x64
+     * Set the URL for the 64x64 pixel portrait.
+     * Ensures the URL uses HTTPS instead of HTTP.
      *
-     * @param string $px64x64
+     * @param string $px64x64 The URL of the 64x64 pixel portrait.
      */
-    protected function setPx64x64(string $px64x64) {
-        $this->px64x64 = \preg_replace('/http:\/\//', 'https://', $px64x64);
+    protected function setPx64x64(string $px64x64): void {
+        $this->px64x64 = preg_replace('/http:\/\//', 'https://', $px64x64);
     }
 
     /**
-     * getPx32x32
-     * -=[ Workaround until CCP implements 32px logos ]=-
+     * Get the URL for the 32x32 pixel portrait.
+     * This is a workaround until 32px portraits are implemented by CCP.
+     * Converts the 64x64 portrait URL to a 32x32 portrait URL by replacing "_64.jpg" with "_32.jpg".
      *
-     * @return string
+     * @return string The URL of the 32x32 pixel portrait.
      */
-    public function getPx32x32() {
-        return \preg_replace('/_64.jpg/', '_32.jpg', $this->px64x64);
+    public function getPx32x32(): string {
+        return preg_replace('/_64.jpg/', '_32.jpg', $this->px64x64);
     }
 }
