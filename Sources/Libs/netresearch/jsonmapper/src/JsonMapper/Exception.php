a937d39296ea0b6812585dc0429f3901ef4661fd
Peter Pfeufer <info@ppfeufer.de>
2025-09-16 15:25:02 +0200

    [CHANGE] Replace JsonMapper with composer package

---
+++ Sources/Libs/netresearch/jsonmapper/src/JsonMapper/Exception.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Simple exception
+ *
+ * @package Ppfeufer_Theme_EVEOnline_JsonMapper
+ * @author  Christian Weiske <cweiske@cweiske.de>
+ * @license OSL-3.0 http://opensource.org/licenses/osl-3.0
+ * @link    http://cweiske.de/
+ */
+class Ppfeufer_Theme_EVEOnline_JsonMapper_Exception extends Exception
+{
+}
+?>
